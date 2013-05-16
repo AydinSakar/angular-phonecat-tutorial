angular/angular-phonecat

step-6 phone images and links
- adding phone image and links to phone pages
- add end2end test that verifies our phone links
- css to style the page just a notch

test/e2e/scenarios.js
@@ -32,5 +32,12 @@ describe('PhoneCat App', function() {
           toEqual(["MOTOROLA XOOM\u2122",
                    "Motorola XOOM\u2122 with Wi-Fi"]);
     });
+
+
+    it('should render phone specific links', function() {
+      input('query').enter('nexus');
+      element('.phones li a').click();
+      expect(browser().location().hash()).toBe('/phones/nexus-s');
+    });
   });
 });

AydinSakar/angular-phonecat-tutorial

test/e2e/scenarios.js
@@ -35,5 +35,12 @@ describe('PhoneCat App', function() {
           toEqual(["MOTOROLA XOOM\u2122",
                    "Motorola XOOM\u2122 with Wi-Fi"]);
     });
+
+
+    it('should render phone specific links', function() {
+      input('query').enter('nexus');
+      element('.phones li a').click();
+      expect(browser().location().url()).toBe('/phones/nexus-s');
+    });
   });
 });